199ae1efd4da43c6feb3032e23e721e5b0f5fd33
nezumi_tech <nezumi_tech@yahoo.co.jp>
2019-03-13 09:59:03 +0900

    Update Hamoron_Firmware.ino

--- Hamoron_Firmware/Hamoron_Firmware.ino
+++ Hamoron_Firmware/Hamoron_Firmware.ino
@@ -6,43 +6,45 @@ const int PWMpin = 5;
 
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
 
-void Initialize() {
-  //全てのピンをLOWにする
-  PORTB &= ~_BV(0);
-  PORTB &= ~_BV(1);
-  PORTB &= ~_BV(2);
-  PORTB &= ~_BV(3);
-  PORTL &= ~_BV(0);
-  PORTL &= ~_BV(1);
-  PORTL &= ~_BV(2);
-  PORTL &= ~_BV(3);
-  PORTL &= ~_BV(4);
-  PORTL &= ~_BV(5);
-  PORTL &= ~_BV(6);
-  PORTL &= ~_BV(7);
-  PORTG &= ~_BV(0);
-  PORTG &= ~_BV(1);
-  PORTG &= ~_BV(2);
-  PORTD &= ~_BV(7);
-  PORTC &= ~_BV(0);
-  PORTC &= ~_BV(1);
-  PORTC &= ~_BV(2);
-  PORTC &= ~_BV(3);
-  PORTC &= ~_BV(4);
-  PORTC &= ~_BV(5);
-  PORTC &= ~_BV(6);
-  PORTC &= ~_BV(7);
-  PORTA &= ~_BV(7);
-  PORTA &= ~_BV(6);
-  PORTA &= ~_BV(5);
-  PORTA &= ~_BV(4);
-  PORTA &= ~_BV(3);
-  PORTA &= ~_BV(2);
-  PORTA &= ~_BV(1);
-  PORTA &= ~_BV(0);
+void Initialize(byte channel, byte number, byte value) {
+  if (number == 120 || number == 123) {
+    //全てのピンをLOWにする
+    PORTB &= ~_BV(0);
+    PORTB &= ~_BV(1);
+    PORTB &= ~_BV(2);
+    PORTB &= ~_BV(3);
+    PORTL &= ~_BV(0);
+    PORTL &= ~_BV(1);
+    PORTL &= ~_BV(2);
+    PORTL &= ~_BV(3);
+    PORTL &= ~_BV(4);
+    PORTL &= ~_BV(5);
+    PORTL &= ~_BV(6);
+    PORTL &= ~_BV(7);
+    PORTG &= ~_BV(0);
+    PORTG &= ~_BV(1);
+    PORTG &= ~_BV(2);
+    PORTD &= ~_BV(7);
+    PORTC &= ~_BV(0);
+    PORTC &= ~_BV(1);
+    PORTC &= ~_BV(2);
+    PORTC &= ~_BV(3);
+    PORTC &= ~_BV(4);
+    PORTC &= ~_BV(5);
+    PORTC &= ~_BV(6);
+    PORTC &= ~_BV(7);
+    PORTA &= ~_BV(7);
+    PORTA &= ~_BV(6);
+    PORTA &= ~_BV(5);
+    PORTA &= ~_BV(4);
+    PORTA &= ~_BV(3);
+    PORTA &= ~_BV(2);
+    PORTA &= ~_BV(1);
+    PORTA &= ~_BV(0);
 
-  //送風量を0にする
-  analogWrite(PWMpin, 0);
+    //送風量を0にする
+    analogWrite(PWMpin, 0);
+  }
 
 }
 
@@ -229,10 +231,9 @@ void setup() {
 
   MIDI.setHandleNoteOn(handleNoteOn);
   MIDI.setHandleNoteOff(handleNoteOff);
-  MIDI.setHandleStop(Initialize);
-  MIDI.setHandleSystemReset(Initialize);
+  MIDI.setHandleControlChange(Initialize);
 
-  Initialize();
+  Initialize(0, 120, 0);
 
   MIDI.begin(1);
   wdt_enable(WDTO_30MS);
